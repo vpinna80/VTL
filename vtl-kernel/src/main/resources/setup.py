653c6b29601589231122d1362e13fa086f3fe736
Valentino Pinna <valentino.pinna@bancaditalia.it>
2020-08-26 17:30:00 +0200

    Jupyter notebook kernel for VTL Engine

---
+++ vtl-kernel/src/main/resources/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="VTL Engine",
+    version="${python.package.version}",
+    author="Valentino Pinna",
+    author_email="valentino.pinna@bancaditalia.it",
+    description="${project.description}",
+    long_description="${project.description}",
+    long_description_content_type="text/plain",
+    url="${site-url}",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "Jpype1", "ipykernel", "numpy", "pandas"
+    ]
+)
