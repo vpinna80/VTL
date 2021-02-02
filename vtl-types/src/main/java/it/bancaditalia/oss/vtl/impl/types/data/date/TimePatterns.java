efb7e810f2fc611c5f5e7ca80416308cddaaa63f
Valentino Pinna <valentino.pinna@bancaditalia.it>
2021-02-02 11:58:07 +0100

    datetime parsing syncronization

--- vtl-types/src/main/java/it/bancaditalia/oss/vtl/impl/types/data/date/TimePatterns.java
+++ vtl-types/src/main/java/it/bancaditalia/oss/vtl/impl/types/data/date/TimePatterns.java
@@ -51,21 +51,21 @@ public class TimePatterns
 	
 	static {
 		PATTERNS.put(Pattern.compile("^(YYYY)(.*)$"), dtf -> dtf.appendValue(YEAR, 4));
-		PATTERNS.put(Pattern.compile("^(YYY)(.*)$"), dtf -> dtf.appendValue(YEAR, 3));
-		PATTERNS.put(Pattern.compile("^(YY)(.*)$"), dtf -> dtf.appendValue(YEAR, 2));
+		PATTERNS.put(Pattern.compile("^(YYY)(?!Y)(.*)$"), dtf -> dtf.appendValue(YEAR, 3));
+		PATTERNS.put(Pattern.compile("^(YY)(?!Y)(.*)$"), dtf -> dtf.appendValue(YEAR, 2));
 		PATTERNS.put(Pattern.compile("^(S)(.*)$"), dtf -> dtf.appendValue(SEMESTER_OF_YEAR, 1));
 		PATTERNS.put(Pattern.compile("^(Q)(.*)$"), dtf -> dtf.appendValue(QUARTER_OF_YEAR, 1));
 		PATTERNS.put(Pattern.compile("^(M[Oo][Nn][Tt][Hh]3)(.*)$"), dtf -> dtf.appendText(MONTH_OF_YEAR, SHORT));
 		PATTERNS.put(Pattern.compile("^(M[Oo][Nn][Tt][Hh]1)(.*)$"), dtf -> dtf.appendText(MONTH_OF_YEAR, NARROW));
-		PATTERNS.put(Pattern.compile("^(M[Oo][Nn][Tt][Hh])(.*)$"), dtf -> dtf.appendText(MONTH_OF_YEAR, FULL));
+		PATTERNS.put(Pattern.compile("^(M[Oo][Nn][Tt][Hh])(?!1|3)(.*)$"), dtf -> dtf.appendText(MONTH_OF_YEAR, FULL));
 		PATTERNS.put(Pattern.compile("^(D[Aa][Yy]3)(.*)$"), dtf -> dtf.appendText(DAY_OF_WEEK, SHORT));
 		PATTERNS.put(Pattern.compile("^(D[Aa][Yy]1)(.*)$"), dtf -> dtf.appendText(DAY_OF_WEEK, NARROW));
-		PATTERNS.put(Pattern.compile("^(D[Aa][Yy])(.*)$"), dtf -> dtf.appendText(DAY_OF_WEEK, FULL));
+		PATTERNS.put(Pattern.compile("^(D[Aa][Yy])(?!1|3)(.*)$"), dtf -> dtf.appendText(DAY_OF_WEEK, FULL));
 		PATTERNS.put(Pattern.compile("^(MM)(.*)$"), dtf -> dtf.appendValue(MONTH_OF_YEAR, 2));
-		PATTERNS.put(Pattern.compile("^(M)(.*)$"), dtf -> dtf.appendValue(MONTH_OF_YEAR, 1, 2, NOT_NEGATIVE));
+		PATTERNS.put(Pattern.compile("^(M)(?!M)(.*)$"), dtf -> dtf.appendValue(MONTH_OF_YEAR, 1, 2, NOT_NEGATIVE));
 		PATTERNS.put(Pattern.compile("^(PPP)(.*)$"), dtf -> dtf.appendValue(DAY_OF_YEAR, 3));
 		PATTERNS.put(Pattern.compile("^(DD)(.*)$"), dtf -> dtf.appendValue(DAY_OF_MONTH, 2));
-		PATTERNS.put(Pattern.compile("^(D)(.*)$"), dtf -> dtf.appendValue(DAY_OF_MONTH, 1, 2, NOT_NEGATIVE));
+		PATTERNS.put(Pattern.compile("^(D)(?!D)(.*)$"), dtf -> dtf.appendValue(DAY_OF_MONTH, 1, 2, NOT_NEGATIVE));
 	}
 
 	public static <T extends TimeValue<?, ?, ?>> TemporalAccessor parseString(String string, String mask)
@@ -80,46 +80,43 @@ public class TimePatterns
 
 	private static DateTimeFormatter getFormatter(final String mask)
 	{
-		DateTimeFormatter formatter = FORMATTERS_CACHE.get(mask);
+		return FORMATTERS_CACHE.computeIfAbsent(mask, TimePatterns::createFormatter);
+	}
+	
+	private static DateTimeFormatter createFormatter(String mask)
+	{
+		String remainingMask = mask;
 		
-		if (formatter == null)
+		// Transform the VTL date mask into a DateTimeFormatter
+		DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+		while (!remainingMask.isEmpty())
 		{
-			String remainingMask = mask;
-			
-			// Transform the VTL date mask into a DateTimeFormatter
-			DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
-			while (!remainingMask.isEmpty())
-			{
-				boolean found = false;
-				for (Pattern pattern: PATTERNS.keySet())
-					if (!found)
-					{
-						Matcher matcher = pattern.matcher(remainingMask);
-						if (matcher.find())
-						{
-							builder = PATTERNS.get(pattern).apply(builder);
-							remainingMask = matcher.group(2);
-							found = true;
-						}
-					}
-				
+			boolean found = false;
+			for (Pattern pattern: PATTERNS.keySet())
 				if (!found)
 				{
-					Matcher matcher = DATE_LITERAL_ELEMENT.matcher(remainingMask);
+					Matcher matcher = pattern.matcher(remainingMask);
 					if (matcher.find())
 					{
-						builder = builder.appendLiteral(matcher.group(1).replaceAll("\\\\", ""));
+						builder = PATTERNS.get(pattern).apply(builder);
 						remainingMask = matcher.group(2);
+						found = true;
 					}
-					else
-						throw new IllegalStateException("Unrecognized mask in csv header: " + remainingMask);
 				}
+			
+			if (!found)
+			{
+				Matcher matcher = DATE_LITERAL_ELEMENT.matcher(remainingMask);
+				if (matcher.find())
+				{
+					builder = builder.appendLiteral(matcher.group(1).replaceAll("\\\\", ""));
+					remainingMask = matcher.group(2);
+				}
+				else
+					throw new IllegalStateException("Unrecognized mask in csv header: " + remainingMask);
 			}
-	
-			formatter = builder.toFormatter();
-			FORMATTERS_CACHE.put(mask, formatter);
 		}
 
-		return formatter;
+		return builder.toFormatter();
 	}
 }
